c2c41097d22917a27b9ea59a62bc4cf9f941368e
Mumapride <mumaprideche@gmail.com>
2026-09-07 05:50:51 +0100

    update IP address

--- mobile/src/api/auth.api.js
+++ mobile/src/api/auth.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://10.70.176.153:5000/api';
+export const BASE_URL = 'http://72.20.10.3:5000/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
